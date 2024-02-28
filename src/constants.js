d155f9933eda7c87433e4a3798c575bde37e53cf
Taichiro-S <hokkaido_mx_1_t.s@me.com>
2024-02-29 01:22:46 +0900

    add slack oauth steps

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,4 @@ export const MONTHLY_RANKING_COUNT = 50
 export const ZENN_ARTICLE_API_ENDPOINT = 'https://zenn.dev/api/articles'
 export const MIN_LIKED_COUNT = 10
 export const ZENN_URL = 'https://zenn.dev'
+export const REDIRECT_URL = 'https://taichiro-s.github.io/zenn_ranking/redirects'
--- src/slack_api.js
+++ src/slack_api.js
@@ -1,4 +1,5 @@
-import { SLACK_WEBHOOK_URL } from './script_property'
+import { SLACK_WEBHOOK_URL, SLACK_APP_CLIENT_ID, SLACK_APP_CLIENT_SECRET } from './script_property'
+import { REDIRECT_URL } from './constants'
 
 export function sendMessageToSlackChannel(message) {
   const payload = JSON.stringify(message)
@@ -8,4 +9,41 @@ export function sendMessageToSlackChannel(message) {
     payload
   }
   UrlFetchApp.fetch(SLACK_WEBHOOK_URL, options)
+}
+
+export function doPost(e) {
+  const payload = JSON.parse(e.postData.contents)
+  const code = payload.code
+
+  const res = UrlFetchApp.fetch('https://slack.com/api/oauth.v2.access', {
+    method: 'post',
+    payload: {
+      code,
+      client_id: SLACK_APP_CLIENT_ID,
+      client_secret: SLACK_APP_CLIENT_SECRET,
+      redirect_uri: REDIRECT_URL
+    }
+  })
+
+  const resJson = JSON.parse(res.getContentText())
+
+  if (resJson.ok) {
+    const teamId = resJson.team.id
+    const appId = resJson.app_id
+    const redirectUrl = `https://slack.com/app_redirect?team=${teamId}&app=${appId}`
+
+    return ContentService.createTextOutput(
+      JSON.stringify({
+        ok: true,
+        redirectUrl
+      })
+    ).setMimeType(ContentService.MimeType.JSON)
+  } else {
+    return ContentService.createTextOutput(
+      JSON.stringify({
+        ok: false,
+        error: '認証に失敗しました。'
+      })
+    ).setMimeType(ContentService.MimeType.JSON)
+  }
 }
