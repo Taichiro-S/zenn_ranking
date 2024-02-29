47a97b2f4412ea13ad116a5c33867e0f9c91a59c
Taichiro-S <hokkaido_mx_1_t.s@me.com>
2024-03-01 00:27:14 +0900

    debug redirect

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,3 @@ export const MONTHLY_RANKING_COUNT = 50
 export const ZENN_ARTICLE_API_ENDPOINT = 'https://zenn.dev/api/articles'
 export const MIN_LIKED_COUNT = 10
 export const ZENN_URL = 'https://zenn.dev'
-export const REDIRECT_URL =
-  'https://script.google.com/macros/s/AKfycbzwus3arL2j-_a2Z0Et4AoeAXxGvKhSpV0JDA5in_kGs8n8j-2xuV1XEPHbwVJ1EuG9Zg/exec'
