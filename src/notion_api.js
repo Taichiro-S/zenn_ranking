a4a435dd37956c60bcfccd8d7633eb3c784fcd0a
Taichiro-S <hokkaido_mx_1_t.s@me.com>
2024-03-02 18:22:15 +0900

    save articles to notion database

--- src/notion_api.js
+++ src/notion_api.js
@@ -1,84 +1,103 @@
-import axios from 'axios'
 import { DEFAULT_EMOJI, MONTHLY_RANKING_COUNT, TIME_PERIOD, WEEKLY_RANKING_COUNT } from './constants'
 import { NOTION_API_KEY, NOTION_DATABASE_PARENT_ID } from './script_property'
 
 const createDatabase = async (period) => {
   const savedAt = new Date().toISOString().split('T')[0]
+  const url = 'https://api.notion.com/v1/databases'
   let databaseName
   if (period === TIME_PERIOD.WEEKLY) {
     databaseName = `週間ランキング_${savedAt}`
   } else if (period === TIME_PERIOD.MONTHLY) {
     databaseName = `月間ランキング_${savedAt}`
   }
-  const response = await axios.post(
-    'https://api.notion.com/v1/databases',
-    {
-      parent: { page_id: NOTION_DATABASE_PARENT_ID },
-      title: [
-        {
-          type: 'text',
-          text: { content: databaseName }
-        }
-      ],
-      properties: {
-        順位: { number: { format: 'number' } },
-        ユーザー: { files: {} },
-        記事タイトル: { title: {} },
-        いいね数: { number: { format: 'number' } },
-        トピック: { multi_select: {} },
-        公開日: { date: {} },
-        記事リンク: { url: {} },
-        ユーザーリンク: { rich_text: {} },
-        ランキング作成日: { date: {} }
+  const payload = {
+    parent: { page_id: NOTION_DATABASE_PARENT_ID },
+    title: [
+      {
+        type: 'text',
+        text: { content: databaseName }
       }
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${NOTION_API_KEY}`,
-        'Notion-Version': '2022-06-28',
-        'Content-Type': 'application/json'
-      }
+    ],
+    properties: {
+      順位: { number: { format: 'number' } },
+      ユーザー: { files: {} },
+      記事タイトル: { title: {} },
+      いいね数: { number: { format: 'number' } },
+      トピック: { multi_select: {} },
+      公開日: { date: {} },
+      記事リンク: { url: {} },
+      ユーザーリンク: { rich_text: {} },
+      ランキング作成日: { date: {} }
     }
-  )
+  }
+
+  const options = {
+    method: 'post',
+    contentType: 'application/json',
+    headers: {
+      Authorization: `Bearer ${NOTION_API_KEY}`,
+      'Notion-Version': '2022-06-28'
+    },
+    payload: JSON.stringify(payload),
+    muteHttpExceptions: true // これにより、例外がスローされずにエラー応答を処理できます
+  }
+
+  const response = UrlFetchApp.fetch(url, options)
+  const statusCode = response.getResponseCode()
+  const content = response.getContentText()
+
+  console.log(statusCode, content)
 
   return response.data
 }
 
 const insertDataIntoDatabase = async (databaseId, article, rank) => {
   const now = new Date()
-  const response = await axios.post(
-    'https://api.notion.com/v1/pages',
-    {
-      parent: { type: 'database_id', database_id: databaseId },
-      icon: { type: 'emoji', emoji: article.emoji },
-      properties: {
-        順位: { number: rank },
-        ユーザー: {
-          files: [{ name: `${article.username}_avatar`, type: 'external', external: { url: article.avatar } }]
-        },
-        記事タイトル: { title: [{ text: { content: article.title } }] },
-        いいね数: { number: article.likedCount },
-        トピック: { multi_select: article.topics.map((topic) => ({ name: topic })) },
-        公開日: { date: { start: article.publishedAt } },
-        記事リンク: { url: article.url },
-        ユーザーリンク: {
-          rich_text: [{ type: 'text', text: { content: article.username, link: { url: article.userLink } } }]
-        },
-        ランキング作成日: { date: { start: now } }
-      }
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${NOTION_API_KEY}`,
-        'Notion-Version': '2022-06-28',
-        'Content-Type': 'application/json'
-      }
+  const url = 'https://api.notion.com/v1/pages'
+  const payload = {
+    parent: { type: 'database_id', database_id: databaseId },
+    icon: { type: 'emoji', emoji: article.emoji },
+    properties: {
+      順位: { number: 1 }, // 例: 順位を1として設定
+      ユーザー: {
+        files: [{ name: `${article.username}_avatar`, type: 'external', external: { url: article.avatar } }]
+      },
+      記事タイトル: { title: [{ text: { content: article.title } }] },
+      いいね数: { number: article.likedCount },
+      トピック: { multi_select: article.topics.map((topic) => ({ name: topic })) },
+      公開日: { date: { start: article.publishedAt } },
+      記事リンク: { url: article.url },
+      ユーザーリンク: {
+        rich_text: [{ type: 'text', text: { content: article.username, link: { url: article.userLink } } }]
+      },
+      ランキング作成日: { date: { start: now } }
     }
-  )
-
-  if (response.status !== 200) {
-    throw new Error(`Failed to create page: ${response.statusText}`)
   }
+
+  const options = {
+    method: 'post',
+    contentType: 'application/json',
+    headers: {
+      Authorization: `Bearer ${NOTION_API_KEY}`,
+      'Notion-Version': '2022-06-28'
+    },
+    payload: JSON.stringify(payload),
+    muteHttpExceptions: true // HTTP例外をミュートに設定
+  }
+
+  const response = UrlFetchApp.fetch(url, options)
+  const responseCode = response.getResponseCode()
+  const responseBody = response.getContentText()
+
+  if (responseCode !== 200) {
+    throw new Error(`Failed to create page: ${responseBody}`)
+  } else {
+    Logger.log(`Article '${article.title}' inserted successfully.`)
+  }
+
+  //   if (response.status !== 200) {
+  //     throw new Error(`Failed to create page: ${response.statusText}`)
+  //   }
 }
 
 export const saveArticlesToNotion = async (articles, period) => {
